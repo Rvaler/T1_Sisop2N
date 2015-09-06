47dad7769d8f95d25f0b945f202305d57a91b8ac
Rafael <rvaler93@gmail.com>
2015-09-06 14:32:04 -0300

    creating matrixes and filling them with the txt values, starting forking processes

--- T1_processVersion.c
+++ T1_processVersion.c
@@ -1,8 +1,29 @@
+/*
+TRABALHO 1 - SISOP 2 N
+
+Rafael Valer - 220489
+Rodrigo Boos - 
+Luis Felipe Polo -
+
+O trabalho consiste em implementar em C (não em C++) um programa concorrente para multiplicação de
+matrizes. O programa deve ser implementado em duas versões diferentes, uma delas utilizando
+processos UNIX e outra utilizando pthreads. Os programas deverão executar obrigatoriamente em
+ambientes Unix (Linux) mesmo que tenham sido desenvolvidos sobre outras plataformas
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/shm.h> 
+#include <sys/stat.h>
+
+FILE *inFileMatrixOne, *inFileMatrixTwo, *outputFileResultMatrix;
 
 int numberRowsMatrixOne, numberColsMatrixOne, numberRowsMatrixTwo, numberColsMatrixTwo;
 int numberOfProcesses; // indicated in the call
+int segmentId;
+int *matrixOne, *matrixTwo, *outputMatrix;
+
+void loadMatrixValues(void);
 
 int main(int argc, char *argv[]){
 
@@ -11,8 +32,10 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 	sscanf(argv[1], "%d", &numberOfProcesses);
-	printf("processos %i\n", numberOfProcesses);
-	FILE *inFileMatrixOne, *inFileMatrixTwo, *outputFileResultMatrix;
+	if (numberOfProcesses <= 0){
+		printf("Error - number of processes must be greater than 0. \n");
+		return -1;
+	}
 
 	//reading matrix files and output creation
 	inFileMatrixOne = fopen("in1.txt", "r");
@@ -32,13 +55,41 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 
-	printf("linhamatriz %d", numberRowsMatrixOne);
-	printf("linhamatriz %d", numberColsMatrixOne);
-	printf("linhamatriz %d", numberRowsMatrixTwo);
-	printf("linhamatriz %d", numberColsMatrixTwo);
+	loadMatrixValues();
+	segmentId = shmget(IPC_PRIVATE, sizeof((*outputMatrix) * numberRowsMatrixOne * numberColsMatrixTwo), S_IRUSR | S_IWUSR);
 
-	
+	int i;
 
+
+	for(i=0; i<numberOfProcesses; i++){
+		pid_t forkId = fork();
+
+		if(forkId < 0){
+			printf("\nError in fork\n");
+			return -1;
+		}else if(forkId > 0){
+
+		}
+	}
 
 	return 0;
+}
+
+// creating and filling matrixes
+void loadMatrixValues(){
+
+	matrixOne = malloc(sizeof(*matrixOne) * numberRowsMatrixOne * numberColsMatrixOne);
+	matrixTwo = malloc(sizeof(*matrixTwo) * numberRowsMatrixTwo * numberColsMatrixTwo);
+
+	int i, j;
+	for(i = 0; i < numberRowsMatrixOne; i++){
+		for(j = 0; j < numberColsMatrixOne; j++){
+			fscanf(inFileMatrixOne, "%d", &matrixOne[i*numberColsMatrixOne + numberRowsMatrixOne]);
+		}
+	}
+	for(i = 0; i < numberRowsMatrixTwo; i++){
+		for(j = 0; j < numberColsMatrixTwo; j++){
+			fscanf(inFileMatrixTwo, "%d", &matrixTwo[i*numberColsMatrixTwo + numberRowsMatrixTwo]);
+		}
+	}
 }
