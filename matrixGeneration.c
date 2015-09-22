a35b3154b800e3ffd50c5bb462462266b9090040
Rvaler <rvaler93@gmail.com>
2015-09-22 17:18:21 -0300

    matrix gen random

--- matrixGeneration.c
+++ matrixGeneration.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 
 int main(int argc, char *argv[]){
 
+	srand( (unsigned)time(NULL) );
 	FILE *in1;
 	int numberOfRows, numberOfCols;
 
@@ -17,10 +18,10 @@ int main(int argc, char *argv[]){
 	int i, j;
 	for (i = 0; i < numberOfRows; i++){
 		for (j = 0; j < numberOfCols; j++){
-			fprintf(in1, "%d ", 2);
+			fprintf(in1, "%d ", rand() % 10);
 		}
 		fprintf(in1, "\n");
 	}
 }
 
-	
+
